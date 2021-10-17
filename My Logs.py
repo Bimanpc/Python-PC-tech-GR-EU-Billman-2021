e668f97d72910851fd12d519b93bb1125247818f
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-10-17 19:44:20 +0300

    Add files via upload

---
+++ My Logs.py
@@ -0,0 +1,6 @@
+import logging
+ 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+logging.info('Billis Logging App started')
+logging.warning('An example logging message.')
+logging.warning('Another log message')

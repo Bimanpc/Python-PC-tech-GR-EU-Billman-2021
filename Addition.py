a2f6df931fb45cfd6112b0952a1e2f41da75d3f6
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-01-15 19:23:02 +0200

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,7 @@
+a=int(input(“enter  first number:”)) //input first number
+
+b=int(input(“enter  second number:”)) //input second number
+
+sum=a+b //addition of two numbers
+
+print(“sum:”, sum) //the required sum is printed
